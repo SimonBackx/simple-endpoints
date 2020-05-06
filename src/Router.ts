f13f973b749030bf4f90c183f6c64258b2dbbf03
Simon Backx <hi@simonbackx.com>
2020-05-06 22:32:03 +0200

    Added wildard route loading

--- src/Router.ts
+++ src/Router.ts
@@ -28,15 +28,35 @@ export class Router {
     endpoints: GenericEndpoint[] = [];
 
     async loadAllEndpoints(folder: string) {
-        const folders = (await fs.readdir(folder, { withFileTypes: true })).filter(dirent => dirent.isDirectory());
+        const parts = folder.split("/");
+        const firstPart = parts.shift();
+        if (firstPart === undefined) {
+            throw new Error("Invalid folder path");
+        }
+        let folderQueue: string[] = [firstPart];
 
-        await asyncForEach(folders, async dirent => {
-            const p = folder + "/" + dirent.name + "/endpoints";
-            if (await directoryExists(p)) {
-                console.log("Endpoints from " + dirent.name);
-                await this.loadEndpoints(p);
+        for (const part of parts) {
+            if (part == "*") {
+                const newQueue: string[] = [];
+                for (folder of folderQueue) {
+                    // Read all directories
+                    const recursiveFolders = (await fs.readdir(folder, { withFileTypes: true }))
+                        .filter((dirent) => dirent.isDirectory())
+                        .map((dirent) => folder + "/" + dirent.name);
+                    newQueue.push(...recursiveFolders);
+                }
+                folderQueue = newQueue;
+            } else {
+                folderQueue = folderQueue.map((folder) => folder + "/" + part);
             }
-        });
+        }
+
+        for (const f of folderQueue) {
+            if (await directoryExists(f)) {
+                console.log("Endpoints from " + f);
+                await this.loadEndpoints(f);
+            }
+        }
     }
 
     /// Run migrations in the given folder
@@ -44,7 +64,7 @@ export class Router {
         /// Query all migrations
         const files = await fs.readdir(folder);
 
-        await asyncForEach(files, async file => {
+        for (const file of files) {
             const p = folder + "/" + file;
             if (file.includes(".test.")) {
                 return;
@@ -57,7 +77,7 @@ export class Router {
                     this.endpoints.push(new element());
                 }
             }
-        });
+        }
     }
 
     async run(request: Request): Promise<EncodedResponse | null> {
