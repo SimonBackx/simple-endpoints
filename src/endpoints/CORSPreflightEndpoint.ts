137c6881277a8818b95c439cc9f4ce835eea294f
Simon Backx <hi@simonbackx.com>
2020-05-07 16:16:56 +0200

    Added basic preflight endpoint

---
+++ src/endpoints/CORSPreflightEndpoint.ts
@@ -0,0 +1,29 @@
+import { Request } from "../Request";
+import { DecodedRequest } from "../DecodedRequest";
+import { Response } from "../Response";
+import { Endpoint } from "../Endpoint";
+
+type Params = {};
+type Query = undefined;
+type Body = undefined;
+type ResponseBody = undefined;
+
+export class CORSPreflightEndpoint extends Endpoint<Params, Query, Body, ResponseBody> {
+    protected doesMatch(request: Request): [true, Params] | [false] {
+        if (request.method != "OPTIONS") {
+            return [false];
+        }
+
+        return [true, {}];
+    }
+
+    async handle(_request: DecodedRequest<Params, Query, Body>) {
+        // todo: improve this a bit
+        const response = new Response(undefined);
+        response.headers["Access-Control-Allow-Origin"] = "*";
+        response.headers["Access-Control-Allow-Methods"] = "*";
+        response.headers["Access-Control-Max-Age"] = "86400"; // Cache 24h
+
+        return Promise.resolve(response);
+    }
+}
