8a28f5313b7aea5c018318e2d8c8e3d8d42dda8a
Simon Backx <hi@simonbackx.com>
2020-05-10 09:56:20 +0200

    Allow all preflight headers

--- src/endpoints/CORSPreflightEndpoint.ts
+++ src/endpoints/CORSPreflightEndpoint.ts
@@ -22,6 +22,7 @@ export class CORSPreflightEndpoint extends Endpoint<Params, Query, Body, Respons
         const response = new Response(undefined);
         response.headers["Access-Control-Allow-Origin"] = "*";
         response.headers["Access-Control-Allow-Methods"] = "*";
+        response.headers["Access-Control-Allow-Headers"] = "*";
         response.headers["Access-Control-Max-Age"] = "86400"; // Cache 24h
 
         return Promise.resolve(response);
